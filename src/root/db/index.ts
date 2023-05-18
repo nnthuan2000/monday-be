a1b0c0afac0bcc3c72731367d83d5b08a7886690
nnthuan <nnthuan2000@gmail.com>
2023-05-18 15:42:54 +0700

    Add more value when get a board

--- src/root/db/index.ts
+++ src/root/db/index.ts
@@ -7,9 +7,9 @@ class Database {
   public db: mongoose.Connection;
 
   private constructor() {
-    // const { password, name } = config.db;
-    // this.uri = `mongodb+srv://monday:${password}@cluster0.gzcsvxt.mongodb.net/${name}?retryWrites=true&w=majority`;
-    this.uri = `mongodb://192.168.1.15:27018/mondayDEV`;
+    const { password, name } = config.db;
+    this.uri = `mongodb+srv://monday:${password}@cluster0.gzcsvxt.mongodb.net/${name}?retryWrites=true&w=majority`;
+    // this.uri = `mongodb://192.168.1.15:27018/mondayDEV`;
     // this.uri = `mongodb://192.168.0.106:27018/mondayDEV`;
 
     if (config.env === 'dev') {
