90f0f18bb6a71f219948e8fb8c8d42fb0a549da6
nnthuan <nnthuan2000@gmail.com>
2023-05-29 09:40:28 +0700

    Fix router get me

--- src/root/app.route.ts
+++ src/root/app.route.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { authenticateV2 } from '../02-authentication/middlewares/authenticate';
-import accessController from '../02-authentication/conttoller';
 import accessRouter from '../02-authentication/route';
+import accessController from '../02-authentication/conttoller';
 
 const router = Router();
 
@@ -9,7 +9,8 @@ router.use('/auth', accessRouter);
 
 // router.use(authenticate as any);
 router.use(authenticateV2 as any);
-router.use('/auth/me', accessController.getMe as any);
-router.use('/auth/logout', accessController.logout as any);
+
+accessRouter.get('/me', accessController.getMe as any);
+accessRouter.post('/logout', accessController.logout as any);
 
 export default router;
