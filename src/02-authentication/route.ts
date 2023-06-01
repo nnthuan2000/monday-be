b3f90071a62f74a3a586175c30e6ca3040c78122
nnthuan <nnthuan2000@gmail.com>
2023-06-01 10:25:45 +0700

    Fix route

--- src/02-authentication/route.ts
+++ src/02-authentication/route.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import accessController from './conttoller';
+import { authenticateV2 } from './middlewares/authenticate';
 
 const accessRouter = Router();
 
@@ -7,5 +8,10 @@ accessRouter.post('/signin', accessController.signIn as any);
 accessRouter.post('/signup', accessController.signUp as any);
 accessRouter.post('/verify', accessController.verifyCode as any);
 accessRouter.post('/sendCode', accessController.sendCode as any);
+
+accessRouter.use(authenticateV2 as any);
+
+accessRouter.get('/me', accessController.getMe as any);
+accessRouter.post('/logout', accessController.logout as any);
 
 export default accessRouter;
--- src/root/app.route.ts
+++ src/root/app.route.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { authenticateV2 } from '../02-authentication/middlewares/authenticate';
 import accessRouter from '../02-authentication/route';
-import accessController from '../02-authentication/conttoller';
 
 const router = Router();
 
@@ -9,8 +8,5 @@ router.use('/auth', accessRouter);
 
 // router.use(authenticate as any);
 router.use(authenticateV2 as any);
-
-accessRouter.get('/me', accessController.getMe as any);
-accessRouter.post('/logout', accessController.logout as any);
 
 export default router;
